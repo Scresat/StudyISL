9e6e844a087aba5a74d795608b027387c6bcd0a2
Satyam Dahiwal <therealsatyam@gmail.com>
2021-06-13 13:27:09 +0530

    quizpage added

--- Dev/Flask/Windows/app.py
+++ Dev/Flask/Windows/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from classifierCommunicator import classifierCommunicator
 from learn import LearnUI
 from home import Home
+from quiz import quiz
 
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ app = Flask(__name__)
 lComm = classifierCommunicator()
 lUI = LearnUI()
 homePage = Home()
+quizObj = quiz()
 
 
 @app.route('/')  # route for homepage
@@ -26,10 +28,14 @@ def learn(symbol):
 def detection():
     return lComm.detection()
 
-@app.route('/quiz')
-def quiz():
-    quiz_list = ["A", "B", "C", "D"]
-    return render_template("quiz.html", quiz_list=quiz_list)
+
+@app.route('/quizpage')
+def quizpage():
+    return quizObj.return_quizpage()
+
+@app.route('/quiz/<id>')
+def quiz(id):
+    return quizObj.return_quiz(int(id))
 
 if __name__ == '__main__':
     app.run()
